222ea21226a157d56ca4cadaaa9ebb9653465686
Amir Afshar <mr.afshar0@gmail.com>
2021-04-13 01:37:09 +0430

    Finalize the hamburger menu functions

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const navTitle = document.getElementsByClassName("nav-heading");
 const subItem = document.getElementsByClassName("sub-item");
+const navBar = document.querySelector(".navbar");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
 
+// Make the Hamburger menu work
+hamburgerMenu.addEventListener("click", function () {
+    navBar.classList.toggle("burger");
+});
+
+// Only in mobile design
 if (window.innerWidth < 768) {
   for (let i = 0; i < navTitle.length; i++) {
     navTitle[i].addEventListener("click", function () {
+      // Showing the contents of the menu and changing the arrow icon Upwards
       navTitle[i].nextElementSibling.classList.toggle("show");
-    });
+      navTitle[i].classList.toggle("active");
+      }
+    );
   }
 }
